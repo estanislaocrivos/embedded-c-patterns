64a7987a1e0183d4b6ceff7c55712d9ec8405bf3
Estanislao Crivos <estanislaocrivos@gmail.com>
2025-07-13 10:45:49 -0300

    Updates.

--- src/minimal_driver.c
+++ src/minimal_driver.c
@@ -20,7 +20,7 @@ void uart_driver_set_rx_callback(uart_rx_isr_callback_t ob)
 void uart_driver_isr(void)
 {
     uint8_t received_byte_mock = 0xFF;
-    printf("UART received byte: 0x%02X\n", received_byte_mock);
+    printf("UART driver received byte: 0x%02X\n", received_byte_mock);
     if (uart_rx_callback)
     {
         printf("Calling callback function from inside UART driver...\n");
